75064cf208de5e54c15ab12b6d7b41f327d7b290
drs317lakshami <devrajkumar317@gmail.com>
2022-11-13 17:28:18 +0530

    Added : Calculated wages for a month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,5 +1,4 @@
 package com.bridgelabz;
-
 public class EmployeeWage {
 
     static final int WAGE_PER_HOUR = 20;
@@ -7,26 +6,29 @@ public class EmployeeWage {
     static final int PART_TIME__HOUR = 4;
     static final int IS_FULL_TIME = 2;
     static final int IS_PART_TIME = 1;
+    static final int WORKING_DAY_PER_MONTH = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program");
         int empPresent = (int) (Math.floor(Math.random() * 10) % 3);
-        int empWage = 0;
+        int dayCount = 1;
+        while (dayCount <= WORKING_DAY_PER_MONTH) {
+            int empWage = 0;
+            switch (empPresent) {
+                case IS_FULL_TIME:
+                    empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    System.out.println("Employee FULL-TIME");
+                    break;
+                case IS_PART_TIME:
+                    empWage = WAGE_PER_HOUR * PART_TIME__HOUR;
+                    System.out.println("Employee PART-TIME");
+                    break;
 
-        switch(empPresent) {
-            case IS_FULL_TIME:
-                empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                System.out.println("Employee FULL-TIME");
-                break;
-
-            case IS_PART_TIME:
-                empWage = WAGE_PER_HOUR * PART_TIME__HOUR;
-                System.out.println("Employee PART-TIME");
-                break;
-
-            default:
-                System.out.println("Employee is Absent");
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            System.out.println("Employee daily wage: " + dayCount + " =>" + empWage);
+            dayCount++;
         }
-        System.out.println("Employee daily wage: " + empWage);
     }
 }
